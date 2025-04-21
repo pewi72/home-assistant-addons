2eab19f76b99144ecb9187d6cc61c14b8b206abe
pewi72 <33689912+pewi72@users.noreply.github.com>
2025-04-21 08:52:01 +0200

    Add files via upload

---
+++ tibber_mqtt/tibber.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+
+print("[TIBBER MQTT DEBUG] Script started")
+print(f"[TIBBER MQTT DEBUG] Arguments: {sys.argv}")
+
+TOKEN = sys.argv[1]
+HOME_ID = sys.argv[2]
+MQTT_HOST = sys.argv[3]
+MQTT_PORT = int(sys.argv[4])
+MQTT_USER = sys.argv[5]
+MQTT_PASS = sys.argv[6]
+
+print(f"[TIBBER MQTT DEBUG] Connecting to MQTT {MQTT_HOST}:{MQTT_PORT} as {MQTT_USER}")
