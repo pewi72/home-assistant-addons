8b2cfcf67911af98addc75198be86c9415610a54
pewi72 <33689912+pewi72@users.noreply.github.com>
2025-04-21 13:07:32 +0200

    Add files via upload

---
+++ tibber_mqtt/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Home Assistant maps your UI fields into these env vars:
+# $TIBBER_TOKEN, $HOME_ID, $MQTT_HOST, $MQTT_PORT, $MQTT_USER, $MQTT_PASSWORD
+
+exec python3 /app/tibber.py   "$TIBBER_TOKEN"   "$HOME_ID"   "$MQTT_HOST"   "$MQTT_PORT"   "$MQTT_USER"   "$MQTT_PASSWORD"
+
